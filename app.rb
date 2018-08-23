409d63813869ab5534f70688d649565d92042b7c
Mathilde Ferrand Coeurderoy <mathilde.ferrand.coeurderoy@gmail.com>
2018-08-23 12:30:12 +0100

    add features test for hitting points

--- app.rb
+++ app.rb
@@ -8,18 +8,19 @@ class Battle < Sinatra::Base
     erb :index
   end
 
-  post "/names" do
+  post '/names' do
     session[:player01] = params[:player01]
     session[:player02] = params[:player02]
-    redirect "/play"
+    redirect '/play'
   end
 
-  get "/play" do
+  get '/play' do
     @player01 = session[:player01]
     @player02 = session[:player02]
     erb :play
   end
 
+  # start the server if ruby file executed directly
   run! if app_file == $0
 
 end
--- spec/features_spec.rb
+++ spec/features_spec.rb
@@ -1,15 +1,26 @@
-feature "Submit names" do
-  
+feature 'Submit names' do
+
   # As two Players,
   # So we can play a personalised game of Battle,
   # We want to Start a fight by entering our names and seeing them
-  scenario "players enter their names" do
-    visit("/")
-    fill_in :player01, with: "Hagrid"
-    fill_in :player02, with: "Dumbledore"
+  scenario 'players enter their names' do
+    visit('/')
+    fill_in :player01, with: 'Hagrid'
+    fill_in :player02, with: 'Dumbledore'
 
-    click_button "Submit"
-    expect(page).to have_content("Hagrid", "Dumbledore")
+    click_button 'Submit'
+    expect(page).to have_content('Hagrid', 'Dumbledore')
   end
+
+    # As Player 1,
+    # So I can see how close I am to winning
+    # I want to see Player 2's Hit Points
+    scenario 'Viewing hit points' do
+      visit('/')
+      fill_in :player01, with: 'Hagrid'
+      fill_in :player02, with: 'Dumbledore'
+      click_button 'Submit'
+      expect(page).to have_content('60HP')
+    end
 
 end
