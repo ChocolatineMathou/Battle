f8ef59f6f7ccf7aa772a9782b8cecde89785b1fe
Mathilde Ferrand Coeurderoy <mathilde.ferrand.coeurderoy@gmail.com>
2018-08-23 14:24:47 +0100

    add attack features test

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -16,4 +16,13 @@ feature 'Submit names' do
       expect(page).to have_content('60HP')
     end
 
+    # As Player 1,
+    # So I can win a game of Battle,
+    # I want to attack Player 2, and I want to get a confirmation
+    scenario 'Attacking Player 2' do
+      sign_in_and_play
+      click_button 'Attack'
+      expect(page).to have_content('You attacked')
+    end
+
 end
